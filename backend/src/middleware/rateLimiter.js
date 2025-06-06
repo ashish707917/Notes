c7c85be3187732156f82215d0ce3ab16c7bcac04
ashish707917 <ashish707917@gmail.com>
2025-06-06 12:49:01 +0530

    initial changes

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,17 @@
+import retelimit from "../config/upstash.js";
+
+
+const rateLimiter = async (req, res, next) => {
+    try {
+        const { success } = await retelimit.limit("my-limit-key");
+
+        if (!success) {
+            return res.status(429).json({message: "Too many request, please try again later"});
+        }
+        next();
+    } catch (error) {
+        console.log("Rate limit error", error);
+        next(error);
+    }
+}
+export default rateLimiter;
